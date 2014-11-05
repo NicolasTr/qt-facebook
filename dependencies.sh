159293ff9811c0cf7518e001e5d06b642da61aec
Nicolas Trésegnie <nicolas.tresegnie@gmail.com>
2014-11-05 19:20:16 +0000

    Add facebook-android-sdk and facebook-ios-sdk to the dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -2,3 +2,9 @@
 
 ./scripts/dependencies-$(uname | tr '[:upper:]' '[:lower:]').sh
 ./scripts/dependencies.sh
+
+export FACEBOOK_SDK_VERSION=3.20.0
+cd vendor
+git clone --branch sdk-version-${FACEBOOK_SDK_VERSION} git@github.com:facebook/facebook-android-sdk.git
+git clone --branch sdk-version-${FACEBOOK_SDK_VERSION} git@github.com:facebook/facebook-ios-sdk.git
+cd ..
